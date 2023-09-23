86b9ade639ac2dc73d8a48cd605ed7d316d858b8
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-09-23 23:40:32 +0400

    Add files via upload

---
+++ webcafe/src/test/java/ssau/web/webcafe/WebcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssau.web.webcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
