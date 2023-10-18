d5e9f21c2e6aa3c4c34570373c0a2c923aebd58c
s3rgey163 <sergeyknyz75@gmail.com>
2023-10-18 17:41:12 +0400

    Add default services

--- webcafe/src/main/java/ru/ssau/webcaffe/service/ProductTypeService.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/service/ProductTypeService.java
@@ -1,7 +1,12 @@
 package ru.ssau.webcaffe.service;
 
-import ru.ssau.webcaffe.pojo.ProductTypePojo;
+import ru.ssau.webcaffe.entity.ProductType;
+import ru.ssau.webcaffe.repo.ProductTypeRepository;
 
-public interface ProductTypeService {
-    ProductTypePojo getProductType(long id);
+public class ProductTypeService implements Service<ProductType, Long> {
+    private ProductTypeRepository productTypeRepository;
+
+    public ProductTypeService(ProductTypeRepository productTypeRepository) {
+        this.productTypeRepository = productTypeRepository;
+    }
 }
