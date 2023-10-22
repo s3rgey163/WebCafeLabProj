c6c552523c7c467a1fcd03ca683b85376cd56a68
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-10-22 22:54:28 +0400

    Add getCustomerByUserId() method in CustomerRepository

--- webcafe/src/main/java/ru/ssau/webcaffe/repo/CustomerRepository.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/repo/CustomerRepository.java
@@ -3,5 +3,8 @@ package ru.ssau.webcaffe.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ssau.webcaffe.entity.Customer;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> getCustomerByUserId(Long id);
 }
