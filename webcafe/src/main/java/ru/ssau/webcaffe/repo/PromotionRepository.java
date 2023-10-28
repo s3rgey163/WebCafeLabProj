76c3b84464116fd40834225f94e40de7038ff2f4
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-10-29 03:29:33 +0400

    Add  type repository methods

--- webcafe/src/main/java/ru/ssau/webcaffe/repo/PromotionRepository.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/repo/PromotionRepository.java
@@ -1,9 +1,17 @@
 package ru.ssau.webcaffe.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ru.ssau.webcaffe.entity.ProductType;
 import ru.ssau.webcaffe.entity.Promotion;
+
+import java.util.List;
 
 @Repository
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+    @Query("from Promotion p where :type in (p.productTypes)")
+    List<Promotion> getAvailablePromosFor(ProductType type);
+
+
 }
