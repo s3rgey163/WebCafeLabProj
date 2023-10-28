56c5e580cc9d6048765e1815c7c24127543f4eef
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-10-29 03:13:36 +0400

    Add product repository methods

--- webcafe/src/main/java/ru/ssau/webcaffe/repo/ProductRepository.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/repo/ProductRepository.java
@@ -2,8 +2,14 @@ package ru.ssau.webcaffe.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.ssau.webcaffe.entity.Category;
 import ru.ssau.webcaffe.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> getByCategory(Category category);
+    List<Product> getByCategoryId(long id);
 }
