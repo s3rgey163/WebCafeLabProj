11ec9808c1171d561cdf6a85498015aebcc7393b
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-11-03 02:24:58 +0400

    Publish an AuthenticationManager bean in WebSecurityConfig

--- webcafe/src/main/java/ru/ssau/webcaffe/security/WebSecurityConfig.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/security/WebSecurityConfig.java
@@ -4,13 +4,18 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.ssau.webcaffe.repo.UserRepository;
@@ -45,9 +50,19 @@ public class WebSecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
--- webcafe/src/main/java/ru/ssau/webcaffe/web/AuthController.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/web/AuthController.java
@@ -1,9 +1,14 @@
 package ru.ssau.webcaffe.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.ssau.webcaffe.security.JWTTokenProvider;
+import ru.ssau.webcaffe.service.UserService;
+import ru.ssau.webcaffe.service.ValidationErrorResponse;
 
 @CrossOrigin
 @RestController
