8d17eaa9f9fa8ff724696899f30ff7dc800ceb24
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-11-15 00:15:39 +0400

    Add AddressRepository.getAddressesByCustomerId()

--- webcafe/src/main/java/ru/ssau/webcaffe/repo/AddressRepository.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/repo/AddressRepository.java
@@ -1,9 +1,15 @@
 package ru.ssau.webcaffe.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.ssau.webcaffe.entity.Address;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    @Query("select c.addresses from Customer c join c.addresses where c.id = :id")
+    List<Address> getAddressesByCustomerId(long id);
 }
--- webcafe/src/main/java/ru/ssau/webcaffe/service/AddressService.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/service/AddressService.java
@@ -1,4 +1,8 @@
 package ru.ssau.webcaffe.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface AddressService {
+
 }
