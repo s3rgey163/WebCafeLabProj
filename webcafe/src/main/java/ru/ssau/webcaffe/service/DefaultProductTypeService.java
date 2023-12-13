1630e60b226979fa157fceb55543ca08d2f7536a
s3rgey163 <85796886+s3rgey163@users.noreply.github.com>
2023-12-14 02:43:39 +0400

    Add createProductType() in DefaultProductService

--- webcafe/src/main/java/ru/ssau/webcaffe/service/DefaultProductTypeService.java
+++ webcafe/src/main/java/ru/ssau/webcaffe/service/DefaultProductTypeService.java
@@ -4,10 +4,14 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import ru.ssau.webcaffe.entity.Product;
 import ru.ssau.webcaffe.entity.ProductType;
+import ru.ssau.webcaffe.entity.Weight;
+import ru.ssau.webcaffe.exception.EntityPersistenceException;
 import ru.ssau.webcaffe.pojo.ProductTypePojo;
+import ru.ssau.webcaffe.repo.ProductRepository;
 import ru.ssau.webcaffe.repo.ProductTypeRepository;
 import ru.ssau.webcaffe.util.Util;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +19,16 @@ import java.util.List;
 @Primary
 public class DefaultProductTypeService {
     private ProductTypeRepository productTypeRepository;
+
+    private ProductRepository productRepository;
+
+    public DefaultProductTypeService(
+            ProductTypeRepository productTypeRepository,
+            ProductRepository productRepository
+    ) {
+        this.productTypeRepository = productTypeRepository;
+        this.productRepository = productRepository;
+    }
 
     public DefaultProductTypeService(ProductTypeRepository productTypeRepository) {
         this.productTypeRepository = productTypeRepository;
@@ -28,5 +42,20 @@ public class DefaultProductTypeService {
     public List<ProductTypePojo> getByProductId(long productId) {
         List<ProductType> productTypes = productTypeRepository.getByProductId(productId);
         return Util.mapCollection(productTypes,  ProductTypePojo::ofEntity, ArrayList::new);
+    }
+
+    public void createProductType(long productId, Weight weight, BigDecimal price, String describe) {
+        Product product = productRepository.findById(productId).orElseThrow(() ->
+                new EntityPersistenceException("Product with id[%d] not found".formatted(productId))
+        );
+        ProductType productType = new ProductType(
+                0,
+                weight,
+                product,
+                price,
+                describe
+        );
+        product.setTypes(List.of(productType));
+        productRepository.save(product);
     }
 }
